c9ac76a6b689d1b1eb28636e66fd7371308b84c2
ShannonJTaylor <shannonjo8475@gmail.com>
2025-03-10 04:41:22 -0400

    Debugged code for render deploy.

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,16 +1,22 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
+//import { model } from 'mongoose';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    console.log("Models object:", models);  // Debugging line
+    console.log(`Trying to access model: ${modelName}`, models[modelName]);  // Debugging line
+
+    
+    let modelExists = await (models[modelName]?.db?.db?.listCollections({
       name: collectionName
-    }).toArray()
+    }).toArray() ?? []);
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
+    console.error("Error in cleanDb, err");
     throw err;
   }
 }
--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,8 +1,10 @@
+
 import db from "../config/connection.js";
-import Question from "../models/Question.js";
+import Question from "../models/Question.js" ;
 import cleanDB from "./cleanDb.js";
 
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+
+const pythonQuestions = await import('./pythonQuestions.json', { assert: { type: 'json' } });
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
