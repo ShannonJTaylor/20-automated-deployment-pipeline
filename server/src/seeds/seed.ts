9664ec89b3ac5e5d4441f5de7d286dd6d268da98
ShannonJTaylor <shannonjo8475@gmail.com>
2025-03-10 05:04:01 -0400

    Updated seed data import.

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -4,7 +4,8 @@ import Question from "../models/Question.js" ;
 import cleanDB from "./cleanDb.js";
 
 
-const pythonQuestions = await import('./pythonQuestions.json', { assert: { type: 'json' } });
+import pythonQuestions from './pythonQuestions.json' assert { type: 'json' };
+
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
